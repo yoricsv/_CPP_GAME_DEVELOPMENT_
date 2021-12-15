c0138966ffe0f7dfe9af23107a66682776e55338
Сергей <yoric@live.ru>
2021-12-15 17:13:43 +0300

    Add MakeTable, change mysqrt.cxx TODO: Fix fault generation Table.h

---
+++ 002_CppCMake/002_6_ComFileGen/MathFunctions/MathFunctions.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef MATH_FUNCTIONS_H
+#define MATH_FUNCTIONS_H
+
+#include <cmath>
+#include <iostream>
+#include "Table.h"
+
+double my_sqrt(double input_value);
+
+#endif // MATH_FUNCTIONS_H
---
+++ 002_CppCMake/002_6_ComFileGen/MathFunctions/mysqrt.cxx
@@ -0,0 +1,61 @@
+#include "MathFunctions.h"
+
+double my_sqrt(double input_value)
+{
+    if(input_value < 0)
+    {
+        std::cerr << "Error! Can't figure out the square root."
+                  << "Your number less than zero."
+                  << std::endl;
+
+        return std::cerr.good() ? EXIT_SUCCESS : EXIT_FAILURE;
+    }
+
+    if(input_value == 0)
+    {
+        return 0;
+    }
+
+    if(input_value == 1)
+    {
+        return 1;
+    }
+
+    // use the table to help find an initial value
+    double result = input_value;
+
+    if (input_value >= 1 && input_value < 10)
+    {
+        std::cout << "Use the table to help find an initial value " << std::endl;
+
+        result = sqrtTable[static_cast<int> (input_value)];
+    }
+
+    // do ten iterations
+    for ( int i = 0;
+          i < 10;
+          ++i
+            )
+    {
+        if (result <= 0)
+        {
+            result = 0.001;
+        }
+
+        double delta = input_value - (result * result);
+
+        result = result + delta / result * 0.5;
+    }
+
+//    while(std::abs(input_value - result * result ) >= 0.001)
+//    {
+//        result = ((input_value / result) + result) / 2;
+//
+//        if(input_value == result * result)
+//        {
+//            return result;
+//        }
+//    }
+
+    return result;
+}
